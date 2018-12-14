0ba99fbbfc46fef0dc73eb97f50173a85fe2acfc
Abhishek Dwevedi <erabhishekdwevedi@users.noreply.github.com>
2018-12-14 14:59:54 +0530

    build

---
+++ react/tutorials/router/app/build/precache-manifest.4e1aaf5ef4f6c5c7faf93c4dc15b4fd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "516a032ca74d36b8d23a",
+    "url": "/static/js/main.516a032c.chunk.js"
+  },
+  {
+    "revision": "a9bbac9eace714c2aa99",
+    "url": "/static/js/1.a9bbac9e.chunk.js"
+  },
+  {
+    "revision": "516a032ca74d36b8d23a",
+    "url": "/static/css/main.f4520133.chunk.css"
+  },
+  {
+    "revision": "a49533d0cf6d4c1e3d8c95892fdea247",
+    "url": "/index.html"
+  }
+];
